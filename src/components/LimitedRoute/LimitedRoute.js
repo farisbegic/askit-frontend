526e63701297b96685ebb6463de8fd5c41628909
farisbegic <faris.begic@stu.ssst.edu.ba>
2022-02-19 17:46:05 +0100

    Added explanation to Limited Routes

--- src/components/LimitedRoute/LimitedRoute.js
+++ src/components/LimitedRoute/LimitedRoute.js
@@ -2,6 +2,7 @@ import React, {useContext} from 'react';
 import {AuthenticationContext} from "../../contexts/AuthenticationContextProvider";
 import {Navigate, Outlet} from "react-router-dom";
 
+// Route used to restrict logged users (Login, Register)
 const LimitedRoute = () => {
     const { rejected } = useContext(AuthenticationContext)
     return rejected ? <Outlet /> : <Navigate to="/"/>
